7a7e5524d0105c0af9fbd9e4e518f37020836f0d
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-05-07 15:41:54 -0300

    mee diooo

--- Assets/ScriptSumador.cs
+++ Assets/ScriptSumador.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScriptSumador : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_InputField numero1Input;
+    public TMP_InputField numero2Input;
+    public TextMeshProUGUI textResultado;
     void Start()
     {
-        
+        textResultado.text = "";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SumarYMostrarResultado()
     {
-        
+        int numero1 = int.Parse(numero1Input.text);
+        int numero2 = int.Parse(numero2Input.text);
+
+        textResultado.text = $"{numero1 + numero2}";    
     }
 }
