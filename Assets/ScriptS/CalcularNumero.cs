e03e18d129496717450b39adc0046abc84cfe76e
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-05-14 15:39:59 -0300

    Ejercicio 6 Funciona

---
+++ Assets/ScriptS/CalcularNumero.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CalcularNumero : MonoBehaviour
+{
+    public TMP_InputField InputNumero;
+    public TextMeshProUGUI resultadoTXT;
+
+    void Start()
+    {
+        resultadoTXT.text = "";
+    }
+
+    public void CalcularTipoNumero()
+    {
+        int numero1 = int.Parse(InputNumero.text);
+        int resto = numero1 % 2;
+        if (numero1 <= 0)
+        {
+            resultadoTXT.text = "El número debe ser mayor a 0";
+            return;
+        }
+        if (resto == 0)
+        {
+            resultadoTXT.text = "El número es par";
+        } else
+        {
+            resultadoTXT.text = "El número es impar";
+        }
+    }
+}
